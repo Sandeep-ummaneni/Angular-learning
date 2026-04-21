e76d30e26cdd83457b740c1d82d076dc61d39d57
sandeep ummaneni <sandeepummaneni24@gmail.com>
2026-04-21 21:49:55 +0530

    Day 4 implemented pipes in project

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { Contentcomponent } from '../content/content';
 import {Spacecraft} from '../spacecraft/spacecraft'
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
-  standalone: true,
- imports: [Contentcomponent,Spacecraft],
+  standalone: true, 
+ imports: [Contentcomponent,Spacecraft,CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class HeaderComponent {
 
-    header : string = "🚀 Deep Space Tracker";
-
+    header : string = "deep space tracker";
+// 🚀
 }
